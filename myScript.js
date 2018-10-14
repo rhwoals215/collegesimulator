0d9d71e143a043819a0ef6646bdb348e27326d85
Jaemin Ko <rhwoals215@gmail.com>
2018-10-13 23:35:04 -0700

    added wrys, listed javascript functions

---
+++ myScript.js
@@ -0,0 +1,43 @@
+function homesleep() {
+
+}
+
+function homeeat() {
+
+}
+
+function homestudy() {
+
+}
+
+function homeworkout() {
+
+}
+
+function checkout() {
+
+}
+
+function applyStarwucks() {
+
+}
+
+function applyWcdonalds() {
+
+}
+
+function applywrys() {
+
+}
+
+function studyingstarwucks() {
+
+}
+
+function flirtStarwucks() {
+
+}
+
+function stealwrys() {
+
+}
