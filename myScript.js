18e64cd002d31312597a211815e3b0e247753e13
Jaemin Ko <rhwoals215@gmail.com>
2018-10-14 17:09:01 -0700

    added time functions, sleeping, display time

--- myScript.js
+++ myScript.js
@@ -23,17 +23,80 @@ function user() {
   this.friends=3;
   this.health=100;
   this.location=0;
-  this.strength=0;
-  this.strength=0;
-
+  this.currentcarttotal=0;
+  this.date=0;
+  this.time=8;
+  this.fatigue=0;
 }
 
 function startnew() {
   new user;
 }
+function displayTime(user) {
+  var ampm="AM";
+  var hour=math.floor(user.time);
+  var minute="00";
+  if (user.time%1!=0){
+    minute="30";
+  }
+  if (user.time>12){
+    ampm="PM";
+    time-=12;
+  }
+  console.log(hour+":"_+minute+ampm);
+  }
+}
 
-function homesleep() {
+function refreshTime(user) {
+  if (user.time>=24){
+    user.time-=24;
+    user.date+=1;
+  }
+}
 
+function homesleep(user) {
+  if (user.fatigue>0){
+    if (user.time<20 && user.time>=8){
+      console.log(displayTime(user.time));
+      console.log("You took a 2 hour nap.");
+      if (user.fatigue<=3){
+        console.log("Fatigue: "+user.fatigue+" -> 0");
+        user.fatigue=0;
+      }
+      else {
+        console.log("Fatigue: "+user.fatigue+" -> "+user.fatigue-3);
+        user.fatigue-=3;
+      }
+      console.log("laziness: "+user.laziness+" -> "+user.laziness+1);
+      user.laziness++;
+    }
+    else if (user.time<2 && user.time>=20) {
+      console.log("You had a great sleep!");
+      console.log("Fatigue: "+user.fatigue+" -> 0");
+      user.fatigue=0;
+      user.time=8;
+    }
+    else if (user.time<8 && user.time>=2){
+      if (user.fatigue<=2){
+        console.log("You slept for a bit.");
+        user.fatigue=0;
+        console.log("Fatigue: "+user.fatigue+" -> 0");
+      }
+      else {
+        console.log("You slept for a bit, but you still feel tired.");
+        console.log("Fatigue: "+user.fatigue+" -> "+user.fatigue-2);
+        user.fatigue-=2;
+      }
+    }
+    else{
+      console.log("You couldn't sleep.");
+    }
+    refreshTime();
+    console.log("Now it is "+displayTime(user.time));
+  }
+  else{
+    console.log("You are not tired");
+  }
 }
 
 function homeeat() {
